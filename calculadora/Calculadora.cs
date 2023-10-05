e7d8bb12230f0b92f7f0c4ca5c071ae8410019a7
Samuel Mendes <75790706+samuelmendespy@users.noreply.github.com>
2023-10-05 20:48:36 -0300

    feat: update operations logic

--- calculadora/Calculadora.cs
+++ calculadora/Calculadora.cs
@@ -10,29 +10,48 @@ namespace calculadora
 {
     public class Calculadora
     {
+        private List<string> listaHistorico;
+
+        public Calculadora()
+        {
+            listaHistorico = new List<string>();
+        }
         public int Somar(int num1, int num2)
         {
-            return 0;
+            listaHistorico.Insert(0, $"Soma {num1} + {num2}");
+            return num1 + num2;
         }
 
         public int Subtrair(int num1, int num2)
         {
-            return 0;
+            listaHistorico.Insert(0, $"Subtração {num1} * {num2}");
+            return num1 - num2;
         }
 
         public int Multiplicar(int num1, int num2)
         {
-            return 0;
+            listaHistorico.Insert(0, $"Multiplicação {num1} * {num2}");
+            return num1 * num2;
         }
 
         public double Dividir(int num1, int num2)
         {
-            return 0.00;
+            try
+            {
+            listaHistorico.Insert(0, $"Divisão {num1} / {num2}");
+            return num1/num2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw ex;
+            }
         }
 
         public List<string> Historico()
         {
-            return new List<string>();
+            // Deixa os 3 primeiros itens da lista e remove os demais
+            listaHistorico.RemoveRange(3, listaHistorico.Count);
+            return listaHistorico;
         }
         
     }   
