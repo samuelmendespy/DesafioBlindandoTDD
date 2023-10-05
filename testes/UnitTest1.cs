1a42ae7cf84adda14a619b3ecbbe561358486efe
Samuel Mendes <75790706+samuelmendespy@users.noreply.github.com>
2023-10-05 19:38:20 -0300

    refact: update test
    changed test UnitTest1 to Somar_InserindoNum1Num2_DeveRetornarSomaDeNum1ComNum2

--- testes/UnitTest1.cs
+++ testes/UnitTest1.cs
@@ -4,9 +4,28 @@ namespace testes;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    private Calculadora _calc;
+    private int num1;
+    private int num2;
+
+    public UnitTest1()
     {
-        Calculadora calc = new Calculadora(); 
+        _calc = new Calculadora();
+    }
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    public void Somar_InserindoNum1Num2_DeveRetornarSomaDeNum1ComNum2(int num1, int num2, int resultadoEsperado)
+    {
+        // Arrange
+        // num1 = 1;
+        // num2 = 2;
+
+        // Act
+       int resultadoAtual = _calc.Somar(num1,num2);
+
+       // Assert
+       Assert.Equal(resultadoEsperado, resultadoAtual);
+
     }
 }
